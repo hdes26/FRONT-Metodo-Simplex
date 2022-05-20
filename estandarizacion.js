ff9548a15b383681d85d26bef9f93387983bc160
Yitzhak928 <yitzhakruiz@hotmail.com>
2022-05-20 03:30:21 -0500

    ya esta realizada la logica de la estandarizacion

--- estandarizacion.js
+++ estandarizacion.js
@@ -6,12 +6,11 @@ let opcion;
 
 //-----------------------------------------------------------------------------------------------------------//
 function enviar(){
-    variables=convertir_numero(document.getElementById('variables').value);
-    restricciones=convertir_numero(document.getElementById('restricciones').value);
+    variables=parseInt(document.getElementById('variables').value);
+    restricciones=parseInt(document.getElementById('restricciones').value);
     opcion=document.getElementById('select_multiples').value;
     create_fun_obj();
     create_restricciones();
-    
 }
 
 //funcion objetivo
@@ -27,49 +26,22 @@ let lista=[];//lista donde se guardara todoa las restricciones sin estandarizar
 let list_ob=[];//lista que contiene la funcion objetivo
 let lista_rectric_estan=[];// esta lista contendra la nueva lista de estandarizacion de las restricciones
 
-leer_fun_obj();//lee la funcion objetivo
-leer_restricciones();// leé las retricciones
-console.log("restricciones")
-console.log(lista);
-
-
-console.log("Opcion:"+opcion);
-switch(opcion){
-    case 1://maximizar
-        document.querySelector('resultado').textContent=Estandarizacion_general(lista,list_ob,"max");
-        console.log(Estandarizacion_general(lista,list_ob,"max"));
-        console.log("restricciones estandarizadas");
-        console.log(lista_rectric_estan);
-        break;
-    case 2://minimizar
-        console.log(Estandarizacion_general(lista,list_ob,"min"));
-        console.log("restricciones estandarizadas");
-        console.log(lista_rectric_estan);
-        break;
-}
-
-
-
 //funcion leer  las restricciones y meterlas en un array
 function leer_restricciones(){
+    const column=document.querySelectorAll('.column')
     for (let i = 0; i <= restricciones-1; i++) {
         //creamos un array que ira´ dentro del array lista
-        console.log("Restriccion:"+(i+1));
+        const data=column[i].querySelectorAll('.row')
         let lista_temp=[];//array
-
         for (let j = 0; j <=variables+1; j++) {
-            let varx=prompt("Restriccion"+(i+1)+"\n"+'x'+(j+1)+":");
-            if(j!=variables){
-                lista_temp.push(convertir_numero(varx));
-            }else{
-                lista_temp.push(varx)
-            }
-            
+            lista_temp.push(data[j].value)    
         }
         lista.push(lista_temp);
     }
 
 }
+
+
 
 // crea los cuadros de donde se obtendra los datos de la funcion objetivo//
 function create_fun_obj(){
@@ -82,9 +54,9 @@ function create_fun_obj(){
   
     for (let index = 0; index <=variables-1; index++) {
         funcion_obj.innerHTML+= `
-        <input type="text" name="" class=""  style="width:5%; height: 10%;">
+        <input type="text" name="" class="obj"  style="width:5%; height: 10%;">
         <label for="">x<sub>${index+1}</sub></label>
-        ${mas(index, variables)}
+        ${mas(index, variables-1)}
         `        
     }
 
@@ -95,40 +67,74 @@ function create_fun_obj(){
 function create_restricciones(){
     const restricciones_input=document.querySelector('.restriccion_div');
     for (let i = 0; i <= restricciones-1; i++) {
-        restricciones_input.innerHTML+=`<p class="input" style="display:flex;"> </p>`
-        const int=document.querySelectorAll('.input');
-        for (let j = 0; j <=(variables+1); j++) {
-            int[i].innerHTML+= `  <input type="text" name="" class=""  style="width:5%; height: 10%;">
-            <label for="">x<sub>${j+1}</sub></label>
-            ${mass(j, variables)}
-            `     
+        restricciones_input.innerHTML+=`<p class="column" style="display:flex;"> </p>`
+        const int=document.querySelectorAll('.column');
+        for (let j = 0; j <=(variables-1); j++) {
+            int[i].innerHTML+= `  <input type="text" name="" class="row"  style="width:5%; height: 10%;">
+             <label for="">x<sub>${j+1}</sub></label>
+             ${mas(j, variables-1)} <label for="" style="width: 1%;"></label>
+            `  
+              
         }
-    }
 
-    
+        for (let index = variables; index <=variables+1 ; index++) {
+          
+            if(index==(variables+1)){
+                int[i].innerHTML+= `  <input type="text" name="" class="row"  style="width:5%; height: 10%;">`
+            }else{
+                int[i].innerHTML+= `  
+                <select name="select" id="select_multiples_m" required class="row">
+                    <option value=">=">>=</option>
+                    <option value="<="><=</option>
+                    <option value="=">=</option>
+                </select>
+                <label for="" style="width: 1%;"></label>
+                    ` 
+            }
+           
+        }
+           
+     }
 }
 
-function crea_inputs(){
-   
-         
-} 
+ function ejecutar(){
+    leer_fun_obj()
+    leer_restricciones()
+    let opcion=parseInt(document.getElementById('select_multiples').value);
+    switch(opcion){
+        case 1://maximizar
+            list_ob=Estandarizacion_general(lista,list_ob,"max");
+            console.log("funcion estandarizada")
+            console.log(list_ob);
+            console.log("restricciones estandarizadas");
+            console.log(lista_rectric_estan);
+            break;
+        case 2://minimizar
+            list_ob=Estandarizacion_general(lista,list_ob,"min");
+            console.log("funcion estandarizada")
+            console.log(list_ob);
+            console.log("restricciones estandarizadas");
+            console.log(lista_rectric_estan);
+            break;
+    }
+ }   
 
 //funcion que leera los datos de entrada de la funcion objetivo y los guardara en el array List
 function leer_fun_obj(){
-    let array_obj=[];//array
-    for (let i = 0; i <=variables-1; i++) {
-        let funcion_obj=parseInt(prompt("funcion objetivo \n"+"x"+(i+1)));
-        array_obj.push(funcion_obj);
+    const fun_obj=document.querySelectorAll('.obj')
+    for (let i = 0; i <=variables-1; i++) {  
+        list_ob.push(fun_obj[i].value);
     }
-    list_ob.push(array_obj);
-    // console.log("funcion objetivo:"+array_obj);
+  
 }
 
 // se realiza el proceso de estandarizacion
 function Estandarizacion_general(lista_res,lista_funobj,max_min){
     
     let Lista_obj=lista_funobj;
+    
     let Lista_t=lista_res;
+    
     let vari="";
 
     if(max_min=="max"){
@@ -142,13 +148,13 @@ function Estandarizacion_general(lista_res,lista_funobj,max_min){
         if(Lista_t[i][variables]==">="){
            OSI++
            MA++
-            Lista_obj[0].push("+0S"+OSI+vari+MA)
+            Lista_obj.push("+0S"+OSI+vari+MA)
         } else if(Lista_t[i][variables]=="<="){
             OSI++
-             Lista_obj[0].push("+0S"+OSI)
+             Lista_obj.push("+0S"+OSI)
         }else if(Lista_t[i][variables]=="="){
             MA++
-             Lista_obj[0].push(vari+MA)
+             Lista_obj.push(vari+MA)
         }
     }
 
@@ -172,26 +178,12 @@ function Estandarizacion_general(lista_res,lista_funobj,max_min){
     return Lista_obj;
 }
 
-
-//fuction convertir numero de string a int
-function convertir_numero(numero){
-    let Numero=parseInt(numero);
-    return Numero;
-}
-
 //validar el signo + despues de cada input
 function mas(index, variable){
-    if(index==(variable-1)){
+    if(index==(variable)){
         return ""
     }else{
       return "+"
     }
 }
 
-function mass(index, variable){
-    if(index==(variable+1)){
-        return ""
-    }else{
-      return "+"
-    }
-}
